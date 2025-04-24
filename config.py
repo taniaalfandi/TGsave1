0d19acfa208bc3b445289b8a483356ad4ce9f7b6
Priyanshu Singh Rajput <147338546+priyanshusingh999@users.noreply.github.com>
2025-04-24 23:49:22 +0530

    Update and rename config.py to Procfile

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-OWNER_ID = int(os.getenv('OWNER_ID'))
-STORAGE_CHANNEL_ID = int(os.getenv('STORAGE_CHANNEL_ID'))
-FORCE_SUB_CHANNEL = os.getenv('FORCE_SUB_CHANNEL')
-BOT_USERNAME = os.getenv('BOT_USERNAME')
