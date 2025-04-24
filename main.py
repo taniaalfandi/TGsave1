49a6f00e96914aebef7d90c1c6ff99fa93625825
Priyanshu Singh Rajput <147338546+priyanshusingh999@users.noreply.github.com>
2025-04-24 23:48:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import requests, time, json, base64
-from config import *
+
+import os
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+OWNER_ID = int(os.getenv('OWNER_ID'))
+STORAGE_CHANNEL_ID = int(os.getenv('STORAGE_CHANNEL_ID'))
+FORCE_SUB_CHANNEL = os.getenv('FORCE_SUB_CHANNEL')
+BOT_USERNAME = os.getenv('BOT_USERNAME')
 
 #=======================================================================[r_ajput999]====================================================================================#
 
