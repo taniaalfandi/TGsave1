84c2c4f65426105c59a4c14fa24718d9d5356f93
Priyanshu Singh Rajput <147338546+priyanshusingh999@users.noreply.github.com>
2025-04-25 23:28:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
+# top of your file
+import threading
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'r_ajput999'
+def home():
+    return 'Bot is running!', 200
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8080)
+
+# run Flask in background thread
+threading.Thread(target=run_flask).start()
 
 
-if __name__ == "__main__":
-    app.run()
+#from flask import Flask
+#app = Flask(__name__)
+
+#@app.route('/')
+#def hello_world():
+ #   return 'r_ajput999'
+
+
+#if __name__ == "__main__":
+#    app.run()
