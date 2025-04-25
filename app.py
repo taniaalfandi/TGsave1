141ab2843f0b5bb12494d8b30d3803d9f87595e0
Priyanshu Singh Rajput <147338546+priyanshusingh999@users.noreply.github.com>
2025-04-25 23:39:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'r_ajput999'
+#@app.route('/')
+#def hello_world():
+#    return 'r_ajput999'
 
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+#    app.run()
