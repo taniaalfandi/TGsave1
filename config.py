aec07b6fe12c5ef8794d583ee2c36630f57c445a
Priyanshu Singh Rajput <147338546+priyanshusingh999@users.noreply.github.com>
2025-04-28 18:34:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-OWNER_ID = os.getenv('2133811513')
+OWNER_ID = int(os.getenv('OWNER_ID'))
 STORAGE_CHANNEL_ID = os.getenv('STORAGE_CHANNEL_ID')
 FORCE_SUB_CHANNEL = os.getenv('FORCE_SUB_CHANNEL')
 BOT_USERNAME = os.getenv('BOT_USERNAME')
